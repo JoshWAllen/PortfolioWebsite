c9d4796309717b8815d8ca1108ac995ff68cd53c
Josh <jwallen883@gmail.com>
2023-02-02 21:49:30 -0500

    updated resume for 2023

--- pages/index.js
+++ pages/index.js
@@ -106,7 +106,7 @@ export default function Home() {
               <li>
                 <a
                   className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
-                  href="/Josh_Allen_Resume_10.13.2022.pdf"
+                  href="/Josh_Allen_Resume_2023.pdf"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
